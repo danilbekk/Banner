f0b80e0e76aabce605ab087f2cedbf3b5e6cb95b
danilbek <deni200040@gmail.com>
2021-08-06 04:38:32 +0300

    added redux folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { store } from "./feature/configureStore";
+import { store } from "./redux/ConfigureStore";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,6 +1,6 @@
 import { combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import currentReducer from "./position";
+import currentReducer from "./features/position";
 export const store = createStore(
   combineReducers({
     current: currentReducer,
